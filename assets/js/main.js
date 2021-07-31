e4cbcef9dd084bbd725fcf6fcec99afc3ddd67b1
thanh <vovanthanhk42cntt@gmail.com>
2021-08-01 01:26:51 +0700

    01082021

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,22 @@
+// Back to top
+$(document).ready(function () {
+  $(window).on("scroll", function () {
+    if ($(this).scrollTop() > 100) {
+      $("#go-to-top").fadeIn();
+    } else {
+      $("#go-to-top").fadeOut();
+    }
+  });
+  $("#go-to-top").click(function () {
+    $("html, body").animate({
+        scrollTop: 0,
+      },
+      2000
+    );
+    return false;
+  });
+});
+
 // Animation
 AOS.init({
     offset: 150,
@@ -30,4 +49,54 @@ $(document).ready(function () {
 
 // food photo
 baguetteBox.run(".gallery");
+
+/*Active navbar click */
+// PC
+$(document).ready(function () {
+  $(".navbar__right ul li a").click(function () {
+    $(".navbar__right ul li a.active").removeClass("active");
+    $(this).addClass("active");
+  });
+});
+// tablet and moblie
+$(document).ready(function () {
+  $(".navbar-list li a").click(function () {
+    $(".navbar-list li a.active").removeClass("active");
+    $(this).addClass("active");
+  });
+});
+
+                                /*List item menu */
+// -----------------------------------------------------------------------------
+$(document).ready(function () {
+  $(".navbar-menu li").click(function () {
+    var choose = $(this).attr("index");
+
+    if (choose == 0) {
+      $(".menu-list__items-drink").show(500);
+      $(".menu-list__items-food").show(500);
+      $(".menu-list__items-seasonal").show(500);
+    } else if (choose == 1) {
+      $(".menu-list__items-drink").show(500);
+      $(".menu-list__items-food").hide(500);
+      $(".menu-list__items-seasonal").hide(500);
+    } else if (choose == 2) {
+      $(".menu-list__items-drink").hide(500);
+      $(".menu-list__items-food").show(500);
+      $(".menu-list__items-seasonal").hide(500);
+    } else {
+      $(".menu-list__items-drink").hide(500);
+      $(".menu-list__items-food").hide(500);
+      $(".menu-list__items-seasonal").show(500);
+    }
+  });
+});
+// click active items
+$(document).ready(function () {
+  $(".navbar-menu li a").click(function () {
+    $(".navbar-menu li a.active").removeClass("active");
+    $(this).addClass("active");
+  });
+});
+
 
